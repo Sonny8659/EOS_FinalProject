c205f716a71e601f1aa177185c0bbd9c073e35c2
Sonny8659 <cksghd321@gmail.com>
2021-12-21 19:44:01 +0900

    first commit

---
+++ Server.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERV_IP "220.149.128.100"
+#define SERV_PORT 4531
+#define BACKLOG 10
+
+#define INIT_MSG "=============================\n Hello! I'm P2P File Sharing Server....\n Please, LOG-IN! \n ============================="
+#define LogIn "***** Log-In Success! *****\n"
+#define LogFail "***** Failed to Log-In :( *****\n"
+#define USER1_ID "user1"
+#define USER1_PW "passwd1"
+#define USER2_ID "user2"
+#define USER2_PW "passwd2"
+
+int main(void)
+{
+	int sockfd, new_fd;
+
+	struct sockaddr_in my_addr;
+	struct sockaddr_in their_addr;
+	unsigned int sin_size;
+
+	pid_t pid;
+
+	int rcv_byte;
+	char buf[512];
+	char List1[512];
+	char List2[512];
+	char id[20];
+	char pw[20];
+	char answer[20];
+	char msg[512];
+	char ip[40];
+	char newlist[1024];
+	char buffer[512];
+
+	int val = 1;
+	int port = 0;
+	int index = 1;
+	int cnt;
+
+	FILE *fp, *de, *so, *deso;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd == -1){
+		perror("Server-socket() error lol!");
+		exit(1);
+    }
+	else printf("Server-socket() sockfd is OK.. \n");
+
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(SERV_PORT);
+	my_addr.sin_addr.s_addr = INADDR_ANY;
+
+	memset(&(my_addr.sin_zero), 0 , 8);
+	
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char*)&val, sizeof(val)) < 0){
+		perror("setsockopt");
+		close(sockfd);
+		return -1;
+	}
+
+    if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1){ 
+		perror("Server-bind() error lol!");
+ 	 	exit(1);											
+	}
+	else printf("Server-bind() is OK .. \n");
+
+
+	 if(listen(sockfd, BACKLOG) == -1){
+	 	perror("listen() error lol!");								    
+	 	exit(1);
+	 }	
+	 else printf("listen() is OK...\n\n");
+
+while(1)
+{
+	 sin_size = sizeof(struct sockaddr_in);
+	 new_fd = accept(sockfd,(struct sockaddr *)&their_addr, &sin_size);
+	
+	 pid = fork();
+
+	 if(pid == 0)
+	 {
+		printf("Accept() is OK.. \n\n");
+		close(sockfd);
+		send(new_fd,INIT_MSG,strlen(INIT_MSG) + 1, 0);
+	 
+		rcv_byte = recv(new_fd, id, sizeof(id), 0);
+		rcv_byte = recv(new_fd, pw, sizeof(pw), 0);
+
+		printf("User Information\n");
+		printf("ID : %s, PW : %s \n\n",id,pw);
+
+		if(!strcmp(id,USER1_ID))
+		{
+			if(!strcmp(pw,USER1_PW))
+			{
+				send(new_fd,LogIn,strlen(LogIn) + 1,0);
+				printf(LogIn);
+
+				//receive file list from client1
+				printf("Do you want receive File List?(Y/N) : ");
+				scanf("%s", &answer);
+				send(new_fd, answer, strlen(answer) + 1, 0);
+				rcv_byte = recv(new_fd, List1, sizeof(List1), 0);
+
+				//write file list on FileList.txt
+				if(!strcmp(answer,"Y")){
+					// update FileList_user1.txt
+					printf("user1 File List : \n%s", List1);
+					fp=fopen("FileList_user1.txt", "w+");
+					fprintf(fp, List1);
+					printf("FileList_user1.txt is Updated!\n");
+				}
+				else {
+					printf("");
+					//fp0 = fopen("FileList.txt", "w+");
+				}
+			}
+			else
+			{
+				send(new_fd, LogFail,strlen(LogFail) + 1,0);
+				printf(LogFail);
+			}
+		}
+
+		if(!strcmp(id,USER2_ID))
+		{
+			if(!strcmp(pw,USER2_PW))
+			{
+				send(new_fd,LogIn,strlen(LogIn) + 1,0);
+				printf(LogIn);
+
+				//receive file list from client1
+				printf("Do you want receive File List?(Y/N) : ");
+				scanf("%s", &answer);
+				send(new_fd, answer, strlen(answer) + 1, 0);
+				rcv_byte = recv(new_fd, List2, sizeof(List2), 0);
+
+				//write file list on FileList.txt
+				if(!strcmp(answer,"Y")){
+					// update FileList_user1.txt
+					printf("user2 File List : \n%s", List2);
+					fp=fopen("FileList_user2.txt", "w+");
+					fprintf(fp, List2);
+					printf("FileList_user2.txt is Updated!\n");
+				}
+				else {
+					printf("");
+				}
+			}
+			else
+			{
+				send(new_fd, LogFail,strlen(LogFail) + 1,0);
+				printf(LogFail);
+			}
+		}
+		if((deso = fopen("FileList.txt","w")) == NULL){printf("error"); return 1;}
+
+		if((de = fopen("FileList_user1.txt","r")) == NULL){printf("FileList_user1 is Empty"); return 1;}
+		else{
+			while((cnt = getc(de)) != EOF) putc(cnt,deso);
+		}
+
+		if((so = fopen("FileList_user2.txt","r")) == NULL){printf("FileList_user2 is Empty"); return 1;}
+		else {
+			while((cnt = getc(so)) != EOF) putc(cnt,deso);
+		}
+
+		
+
+		fclose(de);fclose(so);fclose(deso);
+
+		printf("FileList Updat!\n");
+
+		fclose(fp);
+		close(new_fd);
+		exit(1);
+	 }
+	 if(pid > 0)
+	 {
+		 close(new_fd);
+	 }
+}
+	 close(sockfd);
+	 return 0;
+}
---
+++ client1.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#define SERV_IP "220.149.128.100"
+#define SERV_PORT 4531
+#define CLT1_IP "220.149.128.101"
+#define CLT1_PORT 4532
+#define CLT2_IP "220.149.128.103"
+#define CLT2_PORT 4533
+
+#define LogIn "***** Log-In Success! *****\n"
+
+
+int main(int argc, char *atgv[])
+{
+	//=============Declaration of variables.=================
+	FILE *fd;
+	fd = fopen("ShareList1.txt","w+");
+
+	DIR* dp = NULL;
+	struct dirent* dir = NULL;
+	struct stat DirBuf;
+
+	int sockfd;
+	int index = 1;
+	struct sockaddr_in dest_addr;
+	
+	int rcv_byte;
+
+	char buf[512];
+	char id[20];
+	char pw[20];
+	char List[512] = "";
+	char file_name[10][10];
+	char temp[10] = { 0 };
+
+	//=================================================
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd == -1)
+	{
+		perror("Client-socket() error lol!");
+		exit(1);
+	}
+	else printf("Client-socket() sockfd is OK ... \n");
+
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = htons(SERV_PORT);
+	dest_addr.sin_addr.s_addr = inet_addr(SERV_IP);
+
+	memset(&(dest_addr.sin_zero), 0 , 8);
+
+	if(connect(sockfd,(struct sockaddr *)&dest_addr,sizeof(struct sockaddr)) == -1)
+	{
+		perror("Client-connect() error lol");
+		exit(1);
+	}
+	else printf("Client-connect() is OK... \n\n");
+
+	// Receive Log-in Success message
+	rcv_byte = recv (sockfd, buf, sizeof(buf),0);
+	printf("%s\n", buf);
+
+	printf("ID : ");
+	scanf("%s",id);
+	send(sockfd, id ,strlen(id)+1, 0);
+
+	printf("PW : ");
+	scanf("%s",pw);
+	send(sockfd, pw, strlen(pw)+1, 0);
+
+	rcv_byte = recv(sockfd, buf, sizeof(buf), 0);
+	printf("%s\n", buf);
+	
+	// After Log-in Successe, Send File List to Server
+	if(!strcmp(buf,LogIn))
+	{
+		if((dp = opendir("/home/st2016146025/FinalProject/Share")) == NULL){		// Directory Open Error
+			printf("directroy open error\n");
+			return -1;
+		}
+
+		printf("Here's lists you shared!\n");
+		while((dir = readdir(dp)) != NULL){		// read File from head to end
+			if(!strcmp(".",dir->d_name)||!strcmp("..",dir->d_name))continue;	// . , .. Directory Exception
+			printf("%d : %s \n",index,dir->d_name);		// print Share Directory's Files name
+
+			//sprintf(temp, "%d", index);
+			//strcat(List, "\n======================================\n");
+			//strcat(List, temp);
+			//strcat(List, " : ");
+			strcat(List, dir->d_name);
+			strcat(List,"\t");
+
+			strcat(List, CLT1_IP);
+			strcat(List, "\t");
+
+			sprintf(temp, "%d", CLT1_PORT);
+			strcat(List, temp);
+			strcat(List, "\t");
+
+			strcat(List, "USER1");
+			strcat(List, "\n");
+			index++;
+			/*================================================
+			* Make File List							 	 |
+			-- Send File list it this Form --				 |
+			* ex)							  				 |
+				a.txt CLT1_IP CLT1_PORT			USER1		 |
+				b.txt CLT1_IP CLT1_PORT			USER1		 |
+				c.txt CLT1_IP CLT1_PORT			USER1		 |
+			=================================================*/
+		}
+
+		rcv_byte = recv(sockfd, buf, sizeof(buf), 0);
+		if(!strcmp(buf,"Y")) send(sockfd, List, strlen(List) + 1, 0 );
+		else
+		{
+			// idk
+		}
+		
+
+		printf("Do you want to get a FileList?(yes/no) : ");
+		
+
+		closedir(dp);
+	}
+	close(sockfd);
+
+	return 0;
+}
